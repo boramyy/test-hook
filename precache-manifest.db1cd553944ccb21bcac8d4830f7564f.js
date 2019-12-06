2e5bdb09a7ccd9e42c3536370ec3d156f2d63f2a
boramyy <boramyyo@gmail.com>
2019-12-06 09:45:42 +0900

    Updates

--- precache-manifest.db1cd553944ccb21bcac8d4830f7564f.js
+++ precache-manifest.db1cd553944ccb21bcac8d4830f7564f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "523def4b798fc3446906c72e2e6eefa9",
+    "revision": "9120bc145f5cd74200283749f36d66a8",
     "url": "/test-hook/index.html"
   },
   {
-    "revision": "43d4c0427bdb5d013c8d",
+    "revision": "afd22d232a0c428c7696",
     "url": "/test-hook/static/css/main.78dc1217.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/test-hook/static/js/2.740911ba.chunk.js"
   },
   {
-    "revision": "43d4c0427bdb5d013c8d",
-    "url": "/test-hook/static/js/main.03b21778.chunk.js"
+    "revision": "afd22d232a0c428c7696",
+    "url": "/test-hook/static/js/main.b3eea91a.chunk.js"
   },
   {
     "revision": "fdbf814ced1f3ea66586",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-hook/precache-manifest.178d5fe3339d4029021f8c59c8acb456.js"
+  "/test-hook/precache-manifest.db1cd553944ccb21bcac8d4830f7564f.js"
 );
 
 self.addEventListener('message', (event) => {
